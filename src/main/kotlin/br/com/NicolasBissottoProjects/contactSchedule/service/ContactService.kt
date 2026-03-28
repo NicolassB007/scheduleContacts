daa37f0069572481a4145d1132b2119e24536f5b
Nicolas Bissotto da Silva <nicolasbissottomeusestudos@gmail.com>
2026-03-28 14:24:36 -0300

    methods for registerContact made and getAllContacts made too

---
+++ src/main/kotlin/br/com/NicolasBissottoProjects/contactSchedule/service/ContactService.kt
@@ -0,0 +1,50 @@
+package br.com.NicolasBissottoProjects.contactSchedule.service
+
+import br.com.NicolasBissottoProjects.contactSchedule.dto.request.ContactRequestDto
+import br.com.NicolasBissottoProjects.contactSchedule.dto.response.ContactResponseDto
+import br.com.NicolasBissottoProjects.contactSchedule.model.Contact
+import br.com.NicolasBissottoProjects.contactSchedule.repository.ContactRepository
+import org.springframework.stereotype.Service
+
+@Service
+class ContactService(
+    private val contactRepository: ContactRepository
+) {
+    // Regex contendo o padrão para telefone
+    private val phoneRegex = Regex("""^\(?\d{2}\)?\s?\d{4,5}-\d{4}$""")
+
+    fun registerContact(request: ContactRequestDto): ContactResponseDto {
+        // Validando o telefone antes de salvar
+        require(phoneRegex.matches(request.phone)) {
+            "Telefone inválido: '${request.phone}'. Use o formato (14) 12345-6789"
+        }
+
+        val contact = Contact(
+            name = request.name,
+            phone = request.phone,
+            email = request.email
+        )
+
+        contactRepository.save<Contact>(contact)
+
+        return contact.toResponse()
+    }
+
+    fun getAllContacts(): List<Contact> {
+        return contactRepository.findAll()
+    }
+
+    /*
+    * Extension function para conversão de Contact para ContactRequestDto
+    * O retorno é uma instância de ContactResponseDto
+    * */
+    private fun Contact.toResponse(): ContactResponseDto {
+        return ContactResponseDto(
+            id = id,
+            name = name,
+            email = email,
+            phone = phone,
+            registeredDate = registerDate
+        )
+    }
+}
