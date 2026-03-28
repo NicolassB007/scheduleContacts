c6c60f26e300d86012eeaf7ad45cdadad2d4bf0d
Nicolas Bissotto da Silva <nicolasbissottomeusestudos@gmail.com>
2026-03-28 11:31:16 -0300

    Commiting initial project files

---
+++ src/main/kotlin/br/com/NicolasBissottoProjects/contactSchedule/ContactScheduleApplication.kt
@@ -0,0 +1,11 @@
+package br.com.NicolasBissottoProjects.contactSchedule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContactScheduleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContactScheduleApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/NicolasBissottoProjects/contactSchedule/ContactScheduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.NicolasBissottoProjects.contactSchedule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContactScheduleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
