7cc2309ea0356cb6aa4fe1bb8a75d7539bc706a1
Nicolas Bissotto da Silva <nicolasbissottomeusestudos@gmail.com>
2026-03-28 13:28:43 -0300

    Added annotation 'Repository' at 'ContactRepository' interface

--- src/main/kotlin/br/com/NicolasBissottoProjects/contactSchedule/repository/ContactRepository.kt
+++ src/main/kotlin/br/com/NicolasBissottoProjects/contactSchedule/repository/ContactRepository.kt
@@ -2,6 +2,8 @@ package br.com.NicolasBissottoProjects.contactSchedule.repository
 
 import br.com.NicolasBissottoProjects.contactSchedule.model.Contact
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ContactRepository: JpaRepository<Contact, Long> {
 }
