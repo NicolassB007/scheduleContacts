1b833f142cefbd067096d66cbe1151132cd7b561
Nicolas Bissotto da Silva <nicolasbissottomeusestudos@gmail.com>
2026-03-28 13:12:51 -0300

    Creating Contact model

--- src/main/kotlin/br/com/NicolasBissottoProjects/contactSchedule/model/Contact.kt
+++ src/main/kotlin/br/com/NicolasBissottoProjects/contactSchedule/model/Contact.kt
@@ -1,4 +1,30 @@
 package br.com.NicolasBissottoProjects.contactSchedule.model
 
-class Contact {
-}
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDate
+
+@Entity
+@Table(name = "contacts")
+class Contact(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false)
+    val phone: String,
+
+    @Column(nullable = false)
+    val email: String,
+
+    @Column(nullable = false)
+    val registerDate: LocalDate = LocalDate.now()
+)
